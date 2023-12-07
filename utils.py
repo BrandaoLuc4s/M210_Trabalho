6372b834be59fc00849f8e8b81f002e9540fc88e
BrandaoLuc4s <lucas.brandao@ges.inatel.br>
2023-12-07 18:32:30 -0300

    Finalizado

--- utils.py
+++ utils.py
@@ -1,18 +1,22 @@
 import numpy as np
 
+## Função que está recebendo nRestricoes, nVar, variaveis, restricoes
 def define_coeficientes_restricoes(nRestricoes,nVar,variaveis,restricoes):
     coeficientes_restricoes = []
+    ##Pede pro usuário entrar com as restrições de acordo com os valores das var de decisão
     for i in range(0,nRestricoes):
         print(f'Entrar com as restrições de {restricoes[i]}:')
         restri_dict = {}
         for j in range(0,nVar):
             restri_dict[variaveis[j]] = float(input(f"Entre com o valor do coeficiente de {variaveis[j]}: "))
-        #entrando com lado direito da restrição
+
+        ## Entrando com lado direito da restrição
         restri_dict["LD"] = float(input(f"Entre com o valor do lado direito: "))
         coeficientes_restricoes.append(restri_dict)
     
     string_restricoes = ''
-    #definindo string de restricoes
+
+    #Definindo as variáveis auxiliares de cada restrição
     for i, item in enumerate(coeficientes_restricoes):
         for k, v in item.items():
             if k != 'LD': 
@@ -22,21 +26,22 @@ def define_coeficientes_restricoes(nRestricoes,nVar,variaveis,restricoes):
             else:
                 string_restricoes += f'<= {v} [{restricoes[i]}] \n'  
     
-    #mostrando SA
+    #Retornando as equações formadas das restrições e os coeficientes 
     return coeficientes_restricoes, string_restricoes
 
+## Definindo a matriz primária (primeira tabela simplex)
 def define_matriz_primaria(matriz_linha,matriz_coluna,coeficientes_var,coeficientes_restricoes,variaveis,nVar):
     matriz = np.zeros((matriz_linha,matriz_coluna),dtype='float')
     for i in range(0,matriz_linha):
-        #loop de colunas
+        ##loop de colunas
         for j in range(0,matriz_coluna):
-            #definindo linha Z
+            ##Definindo linha Z
             if i == 0:
                 if j < nVar:
                     matriz[i][j] = coeficientes_var[j] * -1
                 else:
                     matriz[i][j] = 0
-            #definindo linha restricoes
+            ##Definindo a linha das restricoes
             else:
                 if j < nVar:
                     matriz[i][j] = coeficientes_restricoes[i-1][variaveis[j]]
@@ -48,7 +53,7 @@ def define_matriz_primaria(matriz_linha,matriz_coluna,coeficientes_var,coeficien
                     matriz[i][j] = 0
     return matriz
 
-#definindo coluna do pivo
+## Definindo coluna do pivo
 def define_coluna_pivo(matriz,matriz_linha,matriz_coluna):
     i = 0
     menor = 0
@@ -58,7 +63,7 @@ def define_coluna_pivo(matriz,matriz_linha,matriz_coluna):
             coluna_pivo = j
     return coluna_pivo
 
-#definindo linha de referencia
+## Definindo a linha de referencia
 def define_linha_referencia(matriz,matriz_linha,matriz_coluna,coluna_pivo):
     menor_impacto = 999999999999999
     for i in range(1,matriz_linha):
